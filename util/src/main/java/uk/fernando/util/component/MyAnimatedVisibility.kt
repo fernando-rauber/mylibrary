08bfedc7dad9cfee428cb8b2b961d0594d5e93cb
fernandocristiano <75613223+fernandocristiano@users.noreply.github.com>
2023-04-19 21:37:31 +0100

    Libraries updated, Added modifier to MyAnimatedVisibility.

--- util/src/main/java/uk/fernando/util/component/MyAnimatedVisibility.kt
+++ util/src/main/java/uk/fernando/util/component/MyAnimatedVisibility.kt
@@ -4,10 +4,12 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
-fun MyAnimatedVisibility(visible: Boolean, content: @Composable () -> Unit) {
+fun MyAnimatedVisibility(visible: Boolean, modifier: Modifier = Modifier, content: @Composable () -> Unit) {
     AnimatedVisibility(
+        modifier = modifier,
         visible = visible,
         exit = fadeOut(),
         enter = fadeIn()
